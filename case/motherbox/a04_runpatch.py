6a9183051decd0abbc220ec635b22b85d2938493
huilin.xu <huilin.xu@ecoflow.com>
2023-10-13 14:18:03 +0800

    修改exe的执行路径(build/exe.win-amd64-3.11) 修正执行路径的报错

--- case/motherbox/a04_runpatch.py
+++ case/motherbox/a04_runpatch.py
@@ -1,24 +1,27 @@
+import os
+
 from b00_checknet import *
 from b01_checkenvs import *
 
 def do_exe(pathc_dir):
+    """
+    执行脚本
+    :param pathc_dir: 如 D:\autocase\patch\p_ap_match
+    """
     # 查看指定目录下是否有apk文件 且 只有一个apk文件 且 apk文件名是ecoflow_oversea.apk
     files = os.listdir(pathc_dir)  # 获取指定目录下的所有文件
     apk_files = [f for f in files if f.endswith(".apk")]  # 使用列表解析找出所有以".apk"结尾的文件
     if len(apk_files) != 1:  # 检查是否只有一个APK文件且文件名为"ecoflow_oversea.apk"
         tmp_print('x apk文件检查不通过, 请检查脚本目录下是否有且只有一个apk文件')
         return False
-    if apk_files[0] != 'ecoflow_oversea.apk':
-        tmp_print('x apk文件检查不通过, 当前apk文件不是[ecoflow_oversea]的压测版本')
-        return False
 
+    # 进入build/exe.win-amd64-3.11目录
+    exe_build = os.path.join(pathc_dir, 'build', get_pack_dirname())
+    e_files = os.listdir(str(exe_build))  # 获取指定目录下的所有文件
     # 查看是否有exe文件 且 是否以`auto`开头
-    exe_files = [f for f in files if f.endswith(".exe")]  # 使用列表解析找出所有以".exe"结尾的文件
+    exe_files = [f for f in e_files if f.endswith(".exe")]  # 使用列表解析找出所有以".exe"结尾的文件
     if len(exe_files) != 1:  # 检查是否只有一个APK文件且文件名为"ecoflow_oversea.apk"
         tmp_print('x exe文件检查不通过, 请检查脚本目录下是否有且只有一个exe文件')
-        return False
-    if not exe_files[0].startswith('auto'):
-        tmp_print('x exe文件检查不通过, 当前exe文件不是[自动化专项]压测版本')
         return False
 
     # 安装apk文件
@@ -37,7 +40,7 @@ def do_exe(pathc_dir):
 
     # 执行exe文件
     tmp_print('开始执行exe文件...')
-    exe_command = f"{pathc_dir}\\{exe_files[0]}"
+    exe_command = f"{exe_build}\\{exe_files[0]}"
     tmp_print(f'执行exe文件: {exe_command}')
     patch_exe = subprocess.Popen([exe_command], creationflags=subprocess.CREATE_NEW_CONSOLE)
     tmp_print("当前脚本进程ID:", patch_exe.pid)
@@ -51,21 +54,21 @@ def run_patch(func_cancel):
     :param func_cancel:  当前面板下点击取消的回调函数
     """
 
-    # 检查网络
-    is_network_pass = check_all_nets()
-    # 检查环境
-    is_sys_pass = check_all_sys()
-    # 如果网络或者环境变量不通过, 则不执行
-    if not is_network_pass:
-        tmp_print('x 网络检查不通过, 请检查网络')
-        return False
-    if not is_sys_pass:
-        tmp_print('x 环境检查不通过, 请检查环境')
-        return False
-    # 检查是否有脚本目录以及该目录下是否有脚本目录
-    if not os.path.exists(patch_root):
-        tmp_print('x 此前没有下载过任何, 请先回到菜单下载脚本')
-        return False
+    # # 检查网络
+    # is_network_pass = check_all_nets()
+    # # 检查环境
+    # is_sys_pass = check_all_sys()
+    # # 如果网络或者环境变量不通过, 则不执行
+    # if not is_network_pass:
+    #     tmp_print('x 网络检查不通过, 请检查网络')
+    #     return False
+    # if not is_sys_pass:
+    #     tmp_print('x 环境检查不通过, 请检查环境')
+    #     return False
+    # # 检查是否有脚本目录以及该目录下是否有脚本目录
+    # if not os.path.exists(patch_root):
+    #     tmp_print('x 此前没有下载过任何, 请先回到菜单下载脚本')
+    #     return False
 
     # 检查脚本根目录下是否有脚本子目录 {'p_ble_conn_delta': 'D:\\autocase\\patch\\p_ble_conn_delta',}
     patch_cdirs_dict = find_cdirs_prefix(patch_root, patch_cdir_prefix)
@@ -76,7 +79,8 @@ def run_patch(func_cancel):
     patch_cdirs_list = [(str(index), cdirname) for index, cdirname in enumerate(list(patch_cdirs_dict.keys()))]
     # 进入面板选择 '0'
     choice_patch = ['选择要执行的脚本', '请选择:', patch_cdirs_list]
-    patch_seleted = choice_pancel(choice_patch[0], choice_patch[1], choice_patch[2], fun_cancel=func_cancel)
+    # patch_seleted = choice_pancel(choice_patch[0], choice_patch[1], choice_patch[2], fun_cancel=func_cancel)
+    patch_seleted = '0' # todo 2023/10/13 测试阶段默认为'0'
     tmp_print(f'当前选中: {patch_seleted} -> {patch_cdirs_list[int(patch_seleted)]}')
     # 得到绝对路径 'D:\\autocase\\patch\\p_ble_conn_delta'
     local_path = "None"
@@ -93,3 +97,5 @@ def run_patch(func_cancel):
         # 创建脚本目录(不提示,直接创建)
         os.makedirs(local_path)
         return False
+
+run_patch(None)
